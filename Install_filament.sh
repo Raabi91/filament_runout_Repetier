cfb1dd7ed23b0b966c193a2d914576195224ef67
Raabi91 <38133278+Raabi91@users.noreply.github.com>
2018-12-09 10:27:04 +0100

    erer
    dvfdsf

---
+++ Install_filament.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+sudo apt-get purge wiringpi
+hash -r
+
+echo -e "\n\n========= Filament - Installation Script ==========="
+echo -e "\n\nServer Ip:"
+read ip
+echo "Server port"
+read port
+echo "Printer Name"
+read printer
+echo "API Key"
+read apikey
+echo "Sensor (normaly open(1)/closed(0))"
+read sensor
+echo "gpio Pin Use wPi"
+read pin
+
+
+if [ -z "$ip" ] ; then
+    echo 'Server IP Eingeben!'
+    exit 1
+fi
+if [ -z "$port" ] ; then
+    echo 'Server port Eingeben!'
+    exit 1
+fi
+if [ -z "$printer" ] ; then
+    echo 'Printer Name Eingeben!'
+    exit 1
+fi
+if [ -z "$apikey" ] ; then
+    echo 'apikey Eingeben Eingeben!'
+    exit 1
+fi
+if [ -z "$sensor" ] ; then
+    echo 'normaly Open (0) or Closed (1)!'
+    exit 1
+fi
+if [ -z "$pin" ] ; then
+    echo 'Gpio pin Eingeben!'
+    exit 1
+fi
+
+
+sleep 1
+echo "#######Variable#########" > Filament_config.sh
+sleep 1
+echo "ip="$ip"" >> Filament_config.sh
+sleep 1
+echo "port="$port"" >> Filament_config.sh
+sleep 1
+echo "printer="$printer"" >> Filament_config.sh
+sleep 1
+echo "apikey="$apikey"" >> Filament_config.sh
+sleep 1
+echo "sensor="$sensor"" >> Filament_config.sh
+sleep 1
+echo "pin="$pin"" >> Filament_config.sh
+sleep 1
+echo "#######Variable End#########" >> Filament_config.sh
+sleep 1
+chmod 755 filament.sh
+
+
+exit 1
---
+++ filament.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+NAMENDATEI=Filament_config.sh
+. ./$NAMENDATEI
+
+gpio mode $pin up
+gpio mode $pin in
+
+while true
+do
+
+result="$( gpio read $pin )"
+
+if [ "$result" = "$sensor" ]; then
+echo "Filament rounout" 
+curl -k "http://$ip:$port/printer/api/$printer?apikey=$apikey&a=send&data=%7B%22cmd%22%3A%22%40pause%20Filament%20Rounout%22%7D%0A%0A%40pause%20Filament%20Rounout"
+fi
+
+sleep 0.5
+done
